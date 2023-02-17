8dfe8793a102d9175df7b6254390f9819559c113
skdan <skdanish1999@gmail.com>
2023-02-17 17:10:41 +0530

    loading overlay

--- components/LoadingOverlay.js
+++ components/LoadingOverlay.js
@@ -5,12 +5,18 @@ import { userContext } from '../context/userContext'
 
 const LoadingOverlay = () => {
   const {isLoading} = useContext(userContext)
-  if(!isLoading)
-    return null
+  console.log(isLoading)
+  // if(!isLoading)
+  //   return null
   return (
-    <Box w="100vw" h="100vh" fontSize="5xl" fontWeight="bold" display="flex" justifyContent="center" alignItems="center">
-      Loading.....
-    </Box>
+    <>
+    {
+      isLoading &&
+      <Box w="100vw" h="100vh" fontSize="5xl" fontWeight="bold" display="flex" justifyContent="center" alignItems="center">
+        Loading.....
+      </Box>
+    }
+    </>
   )
 }
 
