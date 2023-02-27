e99b5e33b1258e7baa49fc4731f7de16daec86f0
skdan <skdanish1999@gmail.com>
2023-02-27 11:51:10 +0530

    add feed pages

--- pages/feed.js
+++ pages/feed.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { Box, Container, Link, List } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Nav from "../components/Nav";
+import NotesList from "../components/NotesList";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [documents, setDocuments] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:4001/notes");
+      setDocuments(res.data.data);
+      console.log(res.data.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <Box minH="100vh">
+      <Box position="fixed" w="100%" backdropFilter="auto" backdropBlur="2px">
+        <Nav />
+      </Box>
+      <Box pt="70px">
+        <Container maxW={"4xl"}>
+          <List>
+            <NotesList documents={documents} />
+          </List>
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 export default Feed;
