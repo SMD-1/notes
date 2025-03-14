dc2c984c4eba3543d7e4599dfd23b54a4d9df482
Shaikh Mohammad Danish <65220642+SMD-1@users.noreply.github.com>
2025-03-15 01:16:50 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:4001";
-const baseURL = "https://notes.danjs.tech";
+const baseURL = "https://notes.skdan.site";
 const axiosInstance = axios.create({ baseURL });
 
 export default axiosInstance;
